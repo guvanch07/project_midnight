15fdfffacd4932cd29a4d7af255113d99c4c5777
Guvanch Amanov <guvanch.amanov@skystart.team>
2023-04-03 12:19:39 +0300

    changed all routes which have seen on the phone and releted step screen with sign in and sign up

--- main.go
+++ main.go
@@ -1,17 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main() {
-	c := make(chan int, 5)
-	for i := 0; i < 5; i++ {
-		go myFunc(c)
-
-	}
-	fmt.Println(<-c)
+func greet(roc <-chan string) {
+	fmt.Println("Hello " + <-roc + "!")
 }
 
-func myFunc(c chan int) {
+func main() {
+	done := make(chan struct{})
+	go func(c chan struct{}) {
+		go work1(c)
+		close(c)
+	}(done)
+}
 
-	c <- cap(c)
+func work1(c chan struct{}) {
+	fmt.Println("hello")
+
+}
+
+// ///////////////////////////////////
+func workIt(done chan struct{}) {
+	fmt.Println("run work")
+}
+
+func callInputStream() {
+	inputStream := make(chan string)
+	outputStream := make(chan string)
+	go removeDuplicates(inputStream, outputStream)
+	go func() {
+		defer close(inputStream)
+		for _, r := range "112334456" {
+			inputStream <- string(r)
+		}
+	}()
+	for x := range outputStream {
+		fmt.Print(x)
+	}
+}
+
+func removeDuplicates(inputStream, outputStream chan string) {
+	m := make(map[string]string)
+	for v := range inputStream {
+		if m[v] != v {
+			m[v] = v
+			outputStream <- m[v]
+		}
+	}
+	defer close(outputStream)
+}
+
+func task2(c chan string, n string) {
+	for i := 0; i < 5; i++ {
+		c <- n + " "
+		fmt.Print(<-c)
+	}
 }
